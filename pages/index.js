719a9110378c074d8e9cd59e34411772844608e2
Mariana Ferreira <ferreira_mariana@id.uff.br>
2022-08-28 23:38:00 -0300

    Fix warning @next/next/no-page-custom-font

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,6 @@ export default function Home() {
         <title>Advices</title>
         <meta name="description" content="Random advices" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
